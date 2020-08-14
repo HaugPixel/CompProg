a30ce21fdfb6383574b4ede80e322b70cf925c14
gid014 <Erlend.H@student.uib.no>
2020-08-14 17:19:35 +0200

    AustralianVoting.py - adding function to handle input - adding logic to main funtion

--- ProgrammingChallenges/Chapter_1/AustralianVoting/AustralianVoting.py
+++ ProgrammingChallenges/Chapter_1/AustralianVoting/AustralianVoting.py
@@ -1,7 +1,11 @@
 
+def handleInputV1():
+    """
+    Reads Australian election testdoc, and returns election data
+    :return: List of tuples (Candidates, Votes)
+    """
+    cases = []
 
-
-def main():
     with open("easytest.txt", "r") as fil:
         n = int(fil.readline().strip())
         fil.readline()
@@ -9,12 +13,34 @@ def main():
         for x in range(n):
             num = int(fil.readline().strip())
             candidates = [fil.readline().strip() for _ in range(num)]
+            votes = []
             line = None
             while line != "":
                 if line:
-                    votes.append(line.strip())
-                line = fil.readline()
-                vote
+                    votes.append(line)
+                line = fil.readline().strip()
+
+            cases.append((candidates, votes))
+
+    return cases
+
+
+def solveVote(case):
+    """
+    :param case: Tuple (Candidate, Votes)
+    :return: List of winners of election
+    """
+    return []
+
+
+def main():
+    cases = handleInputV1()
+    for case in cases:
+        voteWinners = solveVote(case)
+        for winningCandidate in voteWinners:
+            print(winningCandidate)
+
+
 
 
 if __name__ == '__main__':
